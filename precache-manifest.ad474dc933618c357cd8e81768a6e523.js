d6761556bb707d3ec61fe7aef4379f3ab3e51563
Tymoteusz Wojcik <WojcikTymoteusz9@gmail.com>
2020-08-03 18:42:43 +0200

    Updates

---
+++ precache-manifest.ad474dc933618c357cd8e81768a6e523.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9efac224048fe28c038af690c89ce068",
+    "url": "/currency-converter-react/index.html"
+  },
+  {
+    "revision": "edb9989ae1fafcc24e93",
+    "url": "/currency-converter-react/static/css/main.39a809b3.chunk.css"
+  },
+  {
+    "revision": "15fe4a59ab710cdacedd",
+    "url": "/currency-converter-react/static/js/2.249c0956.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/currency-converter-react/static/js/2.249c0956.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edb9989ae1fafcc24e93",
+    "url": "/currency-converter-react/static/js/main.4bee75be.chunk.js"
+  },
+  {
+    "revision": "2e323cd312abf645af0d",
+    "url": "/currency-converter-react/static/js/runtime-main.6775ec39.js"
+  }
+]);
